xenia-canary/xenia-canary

[GDBStub] Forward `DbgPrint` & `DmSendNotificationString` messages
TODO: see if there's any other debug-string-output functions that we can
pass to debugger?

Might be worth adding a custom GDB command for toggling these too, some
games could be spammy

src/xenia/kernel/xbdm/xbdm_misc.cc
@@ -8,6 +8,7 @@
  */
 
 #include "xenia/base/logging.h"
+#include "xenia/cpu/processor.h"
 #include "xenia/kernel/kernel_state.h"
 #include "xenia/kernel/util/shim_utils.h"
 #include "xenia/kernel/xbdm/xbdm_private.h"
@@ -69,7 +70,14 @@ DECLARE_XBDM_EXPORT1(DmGetXboxName, kDebug, kImplemented)
 dword_result_t DmIsDebuggerPresent_entry() { return 0; }
 DECLARE_XBDM_EXPORT1(DmIsDebuggerPresent, kDebug, kStub);
 
-void DmSendNotificationString_entry(lpdword_t unk0_ptr) {}
+void DmSendNotificationString_entry(lpstring_t message) {
+  XELOGI("(DmSendNotificationString) {}", message.value());
+
+  if (cpu::DebugListener* listener =
+          kernel_state()->processor()->debug_listener()) {
+    listener->OnDebugPrint(message.value());
+  }
+}
 DECLARE_XBDM_EXPORT1(DmSendNotificationString, kDebug, kStub);
 
 dword_result_t DmRegisterCommandProcessor_entry(lpdword_t name_ptr,

0dinD/xenia-canary-netplay

src/xenia/kernel/xbdm/xbdm_misc.cc
@@ -10,6 +10,7 @@
 #include "xenia/kernel/xbdm/xbdm_misc.h"
 #include "xenia/base/logging.h"
 #include "xenia/base/string_util.h"
+#include "xenia/cpu/processor.h"
 #include "xenia/kernel/kernel_state.h"
 #include "xenia/kernel/util/shim_utils.h"
 #include "xenia/kernel/xbdm/xbdm_private.h"
@@ -64,6 +65,13 @@ DECLARE_XBDM_EXPORT1(DmIsDebuggerPresent, kDebug, kStub);
 dword_result_t DmSendNotificationString_entry(lpstring_t notify_string_ptr) {
   if (!notify_string_ptr) {
     return X_E_INVALIDARG;
+  }
+
+  XELOGI("(DmSendNotificationString) {}", notify_string_ptr.value());
+
+  if (cpu::DebugListener* listener =
+          kernel_state()->processor()->debug_listener()) {
+    listener->OnDebugPrint(notify_string_ptr.value());
   }
 
   return XBDM_SUCCESSFUL;